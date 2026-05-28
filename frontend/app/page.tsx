595fc8aa0e214b993535087b329533ef0fea682e
virens27 <virendrashende2019@gmail.com>
2026-05-28 10:21:55 +0530

    Update frontend to use live Render API URL

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 
-const API = "http://127.0.0.1:8000";
+const API = "https://opportunity-tracker-cmqw.onrender.com";
 
 interface Opportunity {
   id: number;
